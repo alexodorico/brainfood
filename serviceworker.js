3df9c7cbbb21ec516e6b43b1c1f6cd77da87d92a
Alex Odorico <alex.odorico@morleynet.com>
2019-06-26 12:22:46 -0400

    Works offline

--- serviceworker.js
+++ serviceworker.js
@@ -1,14 +1,27 @@
+const cacheName = "v1";
+
 self.addEventListener("install", event => {
+  console.log("Service worker installed");
+
   event.waitUntil(
-    caches.open("brainfood-cache").then(cache => {
-      console.log(cache);
+    caches.open(cacheName).then(cache => {
+      console.log('Caching files');
       return cache.addAll(
         [
+          "/",
           "/index.html",
           "/dist/bundle.css",
           "/dist/main.js"
         ]
       );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
