1b25993c905cd5ff6d091bdac6068230308e43bb
alexodorico <alex.odorico@morleynet.com>
2019-06-26 21:38:21 -0400

    Added nescessary emoji

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var connect = require("connect");
 var serveStatic = require("serve-static");
 
 connect().use(serveStatic(__dirname)).listen(8080, _ => {
-    console.log("Server running on port 8080");
+    console.log("Server running on port 8080 🤘");
 });
